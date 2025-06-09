b0236e8498f75da9b9acb09d01fa409a4382ca26
Rafael Meloni <rafaelmeloni@gmail.com>
2025-06-08 22:08:33 -0300

    OrdeItem and OrderItemPK created

--- src/main/java/com/rafaelmeloni/dscommerce/entities/Order.java
+++ src/main/java/com/rafaelmeloni/dscommerce/entities/Order.java
@@ -4,6 +4,8 @@ import com.rafaelmeloni.dscommerce.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table (name = "tb_order")
@@ -24,6 +26,9 @@ public class Order {
 
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
     private Payment payment;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Order() {
     }
